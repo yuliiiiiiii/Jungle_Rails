ed74f9c3b8f599c0138a3cd8e98fabe232a9e772
Yuli Wang <yuliwang1007@gmail.com>
2023-09-26 15:09:50 -0400

    add some comments to understand the project

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,18 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    #now @products will be accessible to index.html.erb template
+
+    #it implicitly renders index.html.erb template:
+    # render :index
+
   end
 
   def show
     @product = Product.find params[:id]
+    #it implicitly renders show.htmml.erb
+    # render :show
+    # only when the name of template does not match with the method, need to write render :XX explicitly
   end
 
 end
